78b2b6199f2f260dbfc1f94a39d0dbfddc374f4a
Adrian Stachlewski <adrian.stachlewski@gemius.com>
2024-11-02 21:16:37 +0100

    More runners

--- ThriftUrls.cmake
+++ ThriftUrls.cmake
@@ -1,4 +1,4 @@
-set(source_url          "https://archive.apache.org/dist/thrift/0.21.0/thrift-0.21.0.tar.gz")
-set(source_sha256              "9a24f3eba9a4ca493602226c16d8c228037db3b9291c6fc4019bfe3bd39fc67c")
-set(windows_binary_url       "https://archive.apache.org/dist/thrift/0.21.0/thrift-0.21.0.exe")
-set(windows_sha256           "e0885da0091e788f2ac9392c54a18ebd2d5526255803ffba10db9a3b4f536f8a")
+set(source_url           "https://archive.apache.org/dist/thrift/0.21.0/thrift-0.21.0.tar.gz")
+set(source_sha256        "9a24f3eba9a4ca493602226c16d8c228037db3b9291c6fc4019bfe3bd39fc67c")
+set(windows_binary_url   "https://archive.apache.org/dist/thrift/0.21.0/thrift-0.21.0.exe")
+set(windows_sha256       "e0885da0091e788f2ac9392c54a18ebd2d5526255803ffba10db9a3b4f536f8a")
--- tests/test_version.py
+++ tests/test_version.py
@@ -1,7 +1,7 @@
 import subprocess
 
 
-def test_compile(tmp_path):
+def test_version():
     stdout = subprocess.check_output(["thrift", "-version"])
     assert stdout.strip() == b'Thrift version 0.21.0'
 
